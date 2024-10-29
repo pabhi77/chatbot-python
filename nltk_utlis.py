31d6df2244635dc508d80c9e45ce52689cb3c1ab
pabhi77 <ayp1436@gmail.com>
2024-10-30 00:55:00 +0530

    Initial commit

---
+++ nltk_utlis.py
@@ -0,0 +1,21 @@
+import nltk
+#nltk.download('punkt')
+#nltk.download('punkt_tab') 
+from nltk.stem.porter import PorterStemmer
+stemmer=PorterStemmer()
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stemm(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(all_words,tokenized_sentence):
+    pass
+
+# stringA="how long does shipping will take?"
+# print(stringA)
+# stringA=tokenize(stringA)
+# print(stringA)
+words=["Organize","organizes","organizing"]
+stemmed_words=[stemm(w)for w in words]
+print(stemmed_words)
